29957eff7bd2c959dff8ce234299ffb1cdf9f832
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-22 21:43:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,12 @@ class School
   attr_accessor :name, :roster
   def initialize(name)
     @name = name
-    @roster = []
+    @roster = {}
+  end
+
+  def add_student(new_name, grade)
+    roster[grade]
+    roster[grade] << new_name
+    
   end
 end
